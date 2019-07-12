c1baecda74e5dc2337b340a7f808a64c0e7b0653
Oleh Babkov <babkoff1989@gmail.com>
2019-07-12 15:51:54 +0300

    round added

--- FirstApp.py
+++ FirstApp.py
@@ -13,3 +13,14 @@ i = 0
 while i <= 20:
     print(i)
     i += 1
+
+firstNum = "1"
+secondNum = 4
+sum = int(firstNum) + secondNum
+print("Sum =", sum)
+
+firstNum = 2.001
+secondNum = 0.1
+sum = firstNum + secondNum
+print(round(sum, 4))
+
