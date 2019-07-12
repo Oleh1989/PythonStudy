ab543ce17c9b1ac6536691dd412a6b8dfb80fe72
Oleh Babkov <babkoff1989@gmail.com>
2019-07-12 15:47:38 +0300

    loops and pow

--- FirstApp.py
+++ FirstApp.py
@@ -1,7 +1,15 @@
-firstName = input("Enter Your first name: ")
-lastName = input("Enter Your last name: ")
+#firstName = input("Enter Your first name: ")
+#lastName = input("Enter Your last name: ")
 
-print("Hello,", firstName, lastName)
+#print("Hello,", firstName, lastName)
 
-print(type(firstName))
-print(type(lastName))
+#print(type(firstName))
+#print(type(lastName))
+
+print(8 ** 2) # возведение в степень
+
+i = 0
+
+while i <= 20:
+    print(i)
+    i += 1
